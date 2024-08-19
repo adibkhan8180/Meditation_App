5106689fd4e6d1da0a8de728193ab21c1a3d9455
Adib Khan <adeebzkhan9@gmail.com>
2024-08-19 16:30:37 +0530

    Adib | Implemented Initial Screen

---
+++ app/index.tsx
@@ -0,0 +1,52 @@
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { CustomButton } from "@/components/CustomButton";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+        >
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-regular text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton onPress={()=> console.log("Tap")} title="Get Started"/>
+            </View>
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ components/CustomButton.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+interface CustomButtonProps {
+  onPress: () => void;
+  title: string;
+  textStyles?: string;
+  containerStyles: string;
+}
+
+export const CustomButton = ({
+  onPress,
+  title,
+  textStyles = "",
+  containerStyles = "",
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${containerStyles}`}
+      onPress={onPress}
+    >
+      <Text className={`font-semibold text-lg ${textStyles}`}> {title} </Text>
+    </TouchableOpacity>
+  );
+};
